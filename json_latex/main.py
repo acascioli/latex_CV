f2e26ac14dedd092f70bd3f2277096948f2ce11d
acascioli <github.com.armoire360@passmail.net>
2024-10-02 13:48:40 +0200

    added the possibility to have variants

--- json_latex/main.py
+++ json_latex/main.py
@@ -10,11 +10,24 @@ from templates.certificates import certificates
 from templates.education import education
 from templates.publications import publications
 from templates.languages import languages
+import shutil
+
+variant = None
+variant = "Bosch"
 
 base_path = plib.Path(__file__).parents[0]
 templates_path = plib.Path(base_path, "templates")
-data_path = plib.Path(base_path, "cv_data")
 resume_path = plib.Path(base_path, "resume")
+file_path = plib.Path(base_path, "resume.tex")
+
+if variant:
+    data_path = plib.Path(base_path, "cv_data_" + variant)
+    file_path_new = plib.Path(base_path, "resume_" + variant + ".tex")
+    shutil.copy(file_path, file_path_new)
+    print(f"File copied from {file_path} to {file_path_new}")
+    file_path = file_path_new
+else:
+    data_path = plib.Path(base_path, "cv_data")
 
 
 P_I(resume_path, data_path)
@@ -35,7 +48,7 @@ languages(resume_path, data_path)
 def run_pdflatex(tex_file):
     try:
         # Execute the pdflatex command
-        subprocess.run(["xelatex", plib.Path(base_path, "resume.tex")], check=True)
+        subprocess.run(["xelatex", file_path], check=True)
         print(f"Successfully compiled {tex_file} to PDF.")
     except subprocess.CalledProcessError as e:
         print(f"Error: pdflatex failed with error code {e.returncode}", file=sys.stderr)
